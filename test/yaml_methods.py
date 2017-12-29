bae3cdc27f55a46718ef2635b7d32e61b458cca7
woodprogrammer <asddeneme3@gmail.com>
2017-12-29 23:41:08 +0200

    Yaml Methods changed

--- test/yaml_methods.py
+++ test/yaml_methods.py
@@ -1,21 +1,36 @@
 import yaml
-def get_environ():
-    pass
-def read_file(key=None):
 
-    with open("./yml_test_files/docker-compose.yml", 'r') as stream:
-        try:
-            yaml_data = yaml.load(stream)
+class ParseCompose:
 
-            if key == None:
-                return yaml_data
-            else:
-                return yaml_data[key]
-        except yaml.YAMLError as exc:
-            print(exc)
+    def __init__(self):
+
+        
+        self.main_data = {}
+        self.services = {}
+        self.service_keywords = {'environment','image','links','depends_on'}
+    def create_service_datas(self):
+        pass
 
 
 
 
+    def get_environ(self):
+        pass
+        
+        
 
-print(read_file(key='services'))
+    def read_file(self,key=None):
+
+        with open("./yml_test_files/docker-compose.yml", 'r') as stream:
+            try:
+                yaml_data = yaml.load(stream)
+                self.main_data = yaml_data
+            except yaml.YAMLError as exc:
+                print(exc)
+
+
+obj = ParseCompose()
+obj.read_file()
+print(obj.get_environ())
+
+
