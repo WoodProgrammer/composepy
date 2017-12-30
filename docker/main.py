4840e954e078c83dac9c411b3a04862e01053641
woodprogrammer <asddeneme3@gmail.com>
2017-12-30 03:16:46 +0200

    Methods Changed

--- docker/main.py
+++ docker/main.py
@@ -1,5 +1,6 @@
 import docker
 import imp 
+import json
 yaml_obj = imp.load_source('*', '../yaml/yaml_methods.py')
 
 class DockerizeBaby:
@@ -12,18 +13,42 @@ class DockerizeBaby:
 
     def connection (self):
         return self.cli.version    
+    
 
-    def create_container(self):
+    def get_service_data(self):
+        return self.service_data
+
+
+
+    def generate_container(self):
+            for k, v in self.service_data.items():
+                print(k  + "Service Elements : " + str(v))
+
+                for i in v.keys():
+                    ## i is the docker element 
+                    ## v[i] is the value of the docker element
+                    print("{}".format(i)+ "\t",v[i])
+                            
+
+
+
+
+
+
+    def set_service_data(self):
     
 #            self.cli.containers.run(image="redis",detach=True)
+     
         self.yaml_worker.read_file(path="/Users/emirozbir/Desktop/composepy/yaml/yml_test_files/docker-compose.yml")
         self.yaml_worker.set_services()
 
-        self.service_data = self.yaml_worker.get_services()
-        return self.service_data
+        service_data = self.yaml_worker.get_services()
+        self.service_data = service_data
 
     def container_count(self):
         return len(self.cli.containers.list())
 
 
-
+obj = DockerizeBaby()
+obj.set_service_data()
+print(obj.generate_container())
