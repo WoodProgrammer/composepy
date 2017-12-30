29edd833bc816bcb270af89e65e06874c438aa34
woodprogrammer <asddeneme3@gmail.com>
2017-12-30 02:28:43 +0200

    Tested docker methods

--- docker/tests/docker_methods_test.py
+++ docker/tests/docker_methods_test.py
@@ -1,16 +1,26 @@
 import unittest
-from main import *
+import imp 
+from main import DockerizeBaby
+
+obj = DockerizeBaby()
 class TestDockerMethods(unittest.TestCase):
 
     def test_connection(self):
-        self.assertTrue(connection())
+        self.assertTrue(obj.connection())
 
     def test_container_creation(self):
-        self.assertTrue(create_container())
+        self.assertTrue(obj.create_container())
 
             
-    def test_container_count(self):      
-        self.assertEqual(3,container_count())
+    #def test_container_count(self):      
+     #   self.assertEqual(3,obj.container_count())
+
+
+    def test_insertion(self):      
+        self.assertTrue(obj.create_container())
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
